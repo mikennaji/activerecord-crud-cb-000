a55a73c0b817f347f163d3b31e97ab68739e6a22
Michael Nnaji <mnnaji3@uwo.ca>
2020-10-16 03:05:03 +0000

    Done.

---
+++ db/migrate/20201016020559_add_columns.rb
@@ -0,0 +1,8 @@
+class AddColumns < ActiveRecord::Migration[5.2]
+  def change
+    add_column :movies, :release_date, :integer
+    add_column :movies, :director, :string
+    add_column :movies, :lead, :string
+    add_column :movies, :in_theaters, :boolean
+  end
+end
