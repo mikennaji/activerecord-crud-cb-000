005e2c4aac835e8d6bf7fda9d74e488562f580da
Michael Nnaji <mnnaji3@uwo.ca>
2020-10-16 02:12:05 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_015820) do
+ActiveRecord::Schema.define(version: 2020_10_16_020559) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
   end
 
 end
