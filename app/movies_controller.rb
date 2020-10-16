9cd91611eb52433d0c4b661b0a22564b7ae04148
Michael Nnaji <mnnaji3@uwo.ca>
2020-10-16 02:19:30 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   movie.save
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
@@ -34,7 +34,7 @@ def can_be_created_in_a_block(args = __)
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  movie = Movie.take(1)
 end
 
 def can_get_the_last_item_in_the_database
